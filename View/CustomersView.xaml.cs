af28e8361a4e5ee2a76d12c9ab92f694ace8280c
David-Stratulat <dstratulat258@gmail.com>
2026-04-02 17:58:46 +0300

    Create WPF coffee shop demo app

---
+++ View/CustomersView.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WiredBrainCoffee.Data;
+using WiredBrainCoffee.ViewModel;
+
+namespace WiredBrainCoffee.View
+{
+
+    public partial class CustomersView : UserControl
+    {
+        private CustomersViewModel _viewModel;
+
+        public CustomersView()
+        {
+            InitializeComponent();
+            _viewModel = new CustomersViewModel(new CustomerDataProvider());
+            DataContext = _viewModel;
+            Loaded += CustomersView_Loaded;
+
+     
+            MoveNavButton.Click += ButtonMoveNavigation_Click;
+        }
+
+        private async void CustomersView_Loaded(object sender, RoutedEventArgs e)
+        {
+            await _viewModel.LoadAsync();
+        }
+
+        private void ButtonMoveNavigation_Click(object sender, RoutedEventArgs e)
+        {
+            var column = Grid.GetColumn(customerListGrid);
+            var newColumn = column == 0 ? 2 : 0;
+            Grid.SetColumn(customerListGrid, newColumn);
+        }
+    }
+}
